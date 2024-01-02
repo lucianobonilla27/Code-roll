5bdded81f051b47069eb7c37b4f276b012775268
Facundo Arebalo <facuar1589@gmail.com>
2024-01-02 15:14:16 -0300

    formateo y simplificacion del código

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
+const canciones = [
+    { id: 1, nombre: "LOS PITS", cantante: "Bad Bunny" },
+    { id: 2, nombre: "VOGUE 787", cantante: "Bad Bunny" },
+    { id: 3, nombre: "Fruto", cantante: "Milo J" },
+    { id: 4, nombre: "hArAkIrI", cantante: "Duki" },
+    { id: 5, nombre: "GIVENCHY", cantante: "Duki" },
+    { id: 6, nombre: "Tus Ojos son de Colores", cantante: "J Balvin" },
+    { id: 7, nombre: "Voy a Decir", cantante: "Maluma" },
+];
+
 function cargarCatalogo() {
-    const canciones = [
-        { id: 1, nombre: "LOS PITS", cantante: "Bad Bunny" },
-        { id: 2, nombre: "VOGUE 787", cantante: "Bad Bunny" },
-        { id: 3, nombre: "Fruto", cantante: "Milo J" },
-        { id: 4, nombre: "hArAkIrI", cantante: "Duki" },
-        { id: 5, nombre: "GIVENCHY", cantante: "Duki" },
-        { id: 6, nombre: "Tus Ojos son de Colores", cantante: "J Balvin" },
-        { id: 7, nombre: "Voy a Decir", cantante: "Maluma" },
-    ];
+    const catalogoCanciones = document.getElementById("catalogoCanciones");
+    catalogoCanciones.innerHTML = "";
 
     canciones.forEach((cancion) => {
         const li = document.createElement("li");
@@ -22,16 +25,6 @@ function buscar() {
     const catalogoCanciones = document.getElementById("catalogoCanciones");
     catalogoCanciones.innerHTML = "";
 
-    const canciones = [
-        { id: 1, nombre: "LOS PITS", cantante: "Bad Bunny" },
-        { id: 2, nombre: "VOGUE 787", cantante: "Bad Bunny" },
-        { id: 3, nombre: "Fruto", cantante: "Milo J" },
-        { id: 4, nombre: "hArAkIrI", cantante: "Duki" },
-        { id: 5, nombre: "GIVENCHY", cantante: "Duki" },
-        { id: 6, nombre: "Tus Ojos son de Colores", cantante: "J Balvin" },
-        { id: 7, nombre: "Voy a Decir", cantante: "Maluma" },
-    ];
-
     for (let i = 0; i < canciones.length; i++) {
         const cancion = canciones[i];
         if (cancion.nombre.toLowerCase().includes(busquedaInput) || cancion.cantante.toLowerCase().includes(busquedaInput)) {
@@ -42,4 +35,7 @@ function buscar() {
         }
     }
 }
+
 cargarCatalogo();
+
+document.getElementById("busquedaInput").addEventListener("input", buscar);

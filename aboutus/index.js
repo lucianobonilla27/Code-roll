914735395e44b3f2dc884c1ae8ed3be7b4d471ae
JulietaRojas <julietar39@gmail.com>
2024-02-03 19:26:47 -0300

    Cambios realizados

---
+++ aboutus/index.js
@@ -0,0 +1,73 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.name', {
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 300,
+    easing: 'cubic-bezier(0.600, -0.550, 0.265, 1.550)'
+});
+
+sr.reveal('.title', {
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 400,
+    easing: 'cubic-bezier(0.600, -0.550, 0.265, 1.550)'
+});
+
+sr.reveal('.subtitle', {
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 500,
+    easing: 'cubic-bezier(0.600, -0.550, 0.265, 1.550)'
+});
+
+sr.reveal('.paragraph', {
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 600,
+    easing: 'cubic-bezier(0.600, -0.550, 0.265, 1.550)'
+});
+
+sr.reveal('.button', {
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 300,
+    easing: 'cubic-bezier(0.600, -0.550, 0.265, 1.550)'
+});
+
+sr.reveal('.img1', {
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 1500,
+    easing: 'cubic-bezier(0.600, -0.550, 0.265, 1.550)'
+});
+
+sr.reveal('.img2', {
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 1500,
+    easing: 'cubic-bezier(0.600, -0.550, 0.265, 1.550)'
+});
+
+sr.reveal('.img3', {
+    duration: 2000,
+    viewFactor: 0.1,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 1500,
+    easing: 'cubic-bezier(0.600, -0.550, 0.265, 1.550)'
+});
